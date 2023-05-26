ce9d7ef6684911d707c161221272951bf4222398
KycTik31 <119877617+KycTik31@users.noreply.github.com>
2023-05-26 15:24:38 +0400

    Update getToken.py

--- modules/getToken.py
+++ modules/getToken.py
@@ -2,6 +2,8 @@ from PyQt6.QtCore import QUrl, QEventLoop, pyqtSignal
 from PyQt6.QtWidgets import QMainWindow, QApplication
 from PyQt6.QtWebEngineWidgets import QWebEngineView
 from configparser import ConfigParser
+from PyQt6 import QtGui
+from PyQt6.QtGui import QAction
 import sys
 
 config = ConfigParser()
@@ -14,10 +16,12 @@ class Token(QMainWindow):
     def __init__(self):
         super().__init__()
         self.token = ""
-
+        self.setWindowIcon(QtGui.QIcon("assets/favicon.png"))
+        self.setWindowTitle("Логин")
         self.browser = QWebEngineView()
         self.setGeometry(740, 160, 450, 800)
         self.setCentralWidget(self.browser)
+        QAction("Quit", self).triggered.connect(self.closeEvent)
         self.browser.setUrl(
             QUrl(
                 "https://oauth.yandex.ru/authorize?response_type=token&client_id=23cabbbdc6cd418abb4b39c32c41195d"))
@@ -29,6 +33,13 @@ class Token(QMainWindow):
         if "#access_token" in q.toString():
             self.token = q.toString().split("=")[1].split("&")[0]
             self.closed.emit()
+
+    def closeEvent(self, event):
+        if len(QApplication.topLevelWidgets()) > 1:
+            self.token = "0"
+            self.closed.emit()
+        else:
+            sys.exit()
 
 
 def UpdateToken():
